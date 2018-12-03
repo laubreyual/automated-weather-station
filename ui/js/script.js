b486a3c9f8583908b414f47c79708082a592ff3f
Wency <wency@docdelta.com>
2018-12-03 14:14:13 +0800

    improve navbar

--- ui/js/script.js
+++ ui/js/script.js
@@ -7,4 +7,8 @@ $(document).ready(function(){
 		$('#editpassword').val($(this).data('password'));
 	});
 	
+	$(".sidebar-toggle").click(function(){
+		$("#app-content").toggleClass("is-collapsed");
+	});
+
 });
